45720634f832b10f7691ed69231c5979c9316b39
Schmali2000 <e52009305@student.tuwien.ac.at>
2026-04-20 22:04:10 +0200

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+from main import annuity_factor, annualized_cost, levelized_cost
+
+
+def test_annuity_factor():
+    assert round(annuity_factor(0.05, 20), 4) == 0.0802
+
+
+def test_annualized_cost():
+    assert round(annualized_cost(10000, 0.05, 20, 200), 2) == 1001.86
+
+
+def test_levelized_cost():
+    assert round(levelized_cost(1000, 5000), 2) == 0.20
